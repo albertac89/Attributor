6835b5a5c7207af0377af7b8e2b322a23b609e54
Albert <albert@alberts-mac.local>
2017-11-02 11:20:44 +0100

    text colours and outline

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -9,19 +9,30 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
 @end
 
 @implementation ViewController
 
+- (IBAction)changeBodySelectionColorToMathcBackgorundOfButton:(UIButton *)sender {
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
+
+- (IBAction)outlineBodySelection {
+    [self.body.textStorage addAttributes:@{ NSStrokeWidthAttributeName: @-3,
+                                            NSStrokeColorAttributeName: [UIColor blackColor]}
+                                   range:self.body.selectedRange];
+}
+- (IBAction)unnoutilineBodySelection {
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName range:self.body.selectedRange];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
