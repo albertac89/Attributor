e24cc43c8ce7a3fe16e79406ea82340b2c040a2c
Albert <albert@alberts-mac.local>
2017-11-03 09:21:28 +0100

    lifecycle

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -11,9 +11,43 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *body;
 @property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 @end
 
 @implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    [title setAttributes:@{NSStrokeWidthAttributeName: @3,
+                           NSStrokeWidthAttributeName: self.outlineButton.tintColor}
+                   range: NSMakeRange(0, [title length])];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(preferredFontsChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification object:nil];
+}
+
+- (void)preferredFontsChanged:(NSNotification *)notification{
+    [self usePreferredFonts];
+}
+
+- (void)usePreferredFonts{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
 
 - (IBAction)changeBodySelectionColorToMathcBackgorundOfButton:(UIButton *)sender {
     [self.body.textStorage addAttribute:NSForegroundColorAttributeName
@@ -26,13 +60,10 @@
                                             NSStrokeColorAttributeName: [UIColor blackColor]}
                                    range:self.body.selectedRange];
 }
+
 - (IBAction)unnoutilineBodySelection {
     [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName range:self.body.selectedRange];
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
 @end
